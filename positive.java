d0b5be85931289351751a3664cf7a90886341bb3
kushagramishra00 <44142171+kushagramishra00@users.noreply.github.com>
2018-10-14 23:05:15 +0530

    Add files via upload

---
+++ positive.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class a
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n>0)
+{
+System.out.println("Positive");
+}
+else
+System.out.println("Negative");
+}
+}
